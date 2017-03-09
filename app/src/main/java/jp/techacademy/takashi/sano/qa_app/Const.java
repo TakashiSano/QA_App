d37d2947b83ffd6c8be45fc0addaf7c0d43285b7
307156 <t-sano@t-gaia.co.jp>
2017-03-09 09:20:35 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/takashi/sano/qa_app/Const.java
@@ -0,0 +1,13 @@
+package jp.techacademy.takashi.sano.qa_app;
+
+/**
+ * Created by 307156 on 2017/03/07.
+ */
+
+public class Const {
+    public static final String UsersPATH = "users"; // Firebaseにユーザの表示名を保存するパス
+    public static final String ContentsPATH = "contents"; // Firebaseに質問を保存するバス
+    public static final String AnswersPATH = "answers"; // Firebaseに解答を保存するパス
+
+    public static final String NameKEY = "name"; // Preferenceに表示名を保存する時のキー
+}
