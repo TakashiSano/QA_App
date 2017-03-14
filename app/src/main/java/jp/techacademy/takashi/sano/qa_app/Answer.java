5f6f0bdfce26aa27fc70a342d5e6f808a3a165bd
307156 <t-sano@t-gaia.co.jp>
2017-03-14 17:42:13 +0900

    途中経過

---
+++ app/src/main/java/jp/techacademy/takashi/sano/qa_app/Answer.java
@@ -0,0 +1,37 @@
+package jp.techacademy.takashi.sano.qa_app;
+
+/**
+ * Created by 307156 on 2017/03/14.
+ */
+
+import java.io.Serializable;
+
+public class Answer implements Serializable {
+    private String mBody;
+    private String mName;
+    private String mUid;
+    private String mAnswerUid;
+
+    public Answer(String body, String name, String uid, String answerUid) {
+        mBody = body;
+        mName = name;
+        mUid = uid;
+        mAnswerUid = answerUid;
+    }
+
+    public String getBody() {
+        return mBody;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getUid() {
+        return mUid;
+    }
+
+    public String getAnswerUid() {
+        return mAnswerUid;
+    }
+}
