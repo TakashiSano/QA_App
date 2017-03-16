0ea228b754cc81fe56de778e85f91f84d786e716
307156 <t-sano@t-gaia.co.jp>
2017-03-16 19:34:17 +0900

    課題途中

--- app/src/main/java/jp/techacademy/takashi/sano/qa_app/QuestionDetailActivity.java
+++ app/src/main/java/jp/techacademy/takashi/sano/qa_app/QuestionDetailActivity.java
@@ -1,11 +1,17 @@
 package jp.techacademy.takashi.sano.qa_app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.ListView;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -17,8 +23,6 @@ import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.HashMap;
 
-import static jp.techacademy.takashi.sano.qa_app.R.id.switch1;
-
 public class QuestionDetailActivity extends AppCompatActivity {
 
     private ListView mListView;
@@ -26,6 +30,12 @@ public class QuestionDetailActivity extends AppCompatActivity {
     private QuestionDetailListAdapter mAdapter;
 
     private DatabaseReference mAnswerRef;
+
+    //佐野が追加した
+    private Switch mSwitch = null;
+    SharedPreferences mPreference;
+    //佐野が追加した
+
 
     private ChildEventListener mEventListener = new ChildEventListener() {
         @Override
@@ -71,9 +81,10 @@ public class QuestionDetailActivity extends AppCompatActivity {
         }
     };
 
+    // 佐野が追加した
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onRestart() {
+        super.onRestart();
         setContentView(R.layout.activity_question_detail);
 
         // 渡ってきたQuestionのオブジェクトを保持する
@@ -82,7 +93,7 @@ public class QuestionDetailActivity extends AppCompatActivity {
 
         setTitle(mQuestion.getTitle());
 
-        // 佐野が追加した
+
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
         if (user == null) {
@@ -94,7 +105,6 @@ public class QuestionDetailActivity extends AppCompatActivity {
             setContentView(R.layout.activity_question_detail);
             findViewById(R.id.switch1).setVisibility(View.VISIBLE);
         }
-        // 佐野が追加した
 
         // ListViewの準備
         mListView = (ListView) findViewById(R.id.listView);
@@ -125,5 +135,129 @@ public class QuestionDetailActivity extends AppCompatActivity {
         DatabaseReference dataBaseReference = FirebaseDatabase.getInstance().getReference();
         mAnswerRef = dataBaseReference.child(Const.ContentsPATH).child(String.valueOf(mQuestion.getGenre())).child(mQuestion.getQuestionUid()).child(Const.AnswersPATH);
         mAnswerRef.addChildEventListener(mEventListener);
+
+        mSwitch = (Switch)findViewById(R.id.switch1);
+
+        //Switch保持のために追加
+        mPreference = PreferenceManager.getDefaultSharedPreferences(this);
+        //Switch保持のために追加
+
+        mSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mSwitch = (Switch)findViewById(R.id.switch1);
+
+                if(mSwitch.isChecked()) {
+                    Log.v("スイッチ", "ON");
+                    //Switch保持のために追加
+                    SharedPreferences.Editor editor = mPreference.edit();
+                    editor.putBoolean("スイッチ",true);
+                    editor.commit();
+                    //Switch保持のために追加
+                    Toast.makeText(QuestionDetailActivity.this, "お気に入りに追加しました。", Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.v("スイッチ", "OFF");
+                    //Switch保持のために追加
+                    SharedPreferences.Editor editor = mPreference.edit();
+                    editor.putBoolean("スイッチ",false);
+                    editor.commit();
+                    //Switch保持のために追加
+                    Toast.makeText(QuestionDetailActivity.this, "お気に入りから削除しました。", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+    //佐野が追加した
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_detail);
+
+        // 渡ってきたQuestionのオブジェクトを保持する
+        Bundle extras = getIntent().getExtras();
+        mQuestion = (Question) extras.get("question");
+
+        setTitle(mQuestion.getTitle());
+
+        // 佐野が追加した
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user == null) {
+            // ログインしていなければswitchを消す
+            setContentView(R.layout.activity_question_detail);
+            findViewById(R.id.switch1).setVisibility(View.INVISIBLE);
+        } else {
+            // ログインしていればswitchを表示
+            setContentView(R.layout.activity_question_detail);
+            findViewById(R.id.switch1).setVisibility(View.VISIBLE);
+        }
+        // 佐野が追加した
+
+
+
+        // ListViewの準備
+        mListView = (ListView) findViewById(R.id.listView);
+        mAdapter = new QuestionDetailListAdapter(this, mQuestion);
+        mListView.setAdapter(mAdapter);
+        mAdapter.notifyDataSetChanged();
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // ログイン済みのユーザーを収録する
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                if (user == null) {
+                    // ログインしていなければログイン画面に遷移させる
+                    Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                    startActivity(intent);
+                } else {
+                    // Questionを渡して回答作成画面を起動する
+                    Intent intent = new Intent(getApplicationContext(), AnswerSendActivity.class);
+                    intent.putExtra("question", mQuestion);
+                    startActivity(intent);
+                }
+            }
+        });
+
+        DatabaseReference dataBaseReference = FirebaseDatabase.getInstance().getReference();
+        mAnswerRef = dataBaseReference.child(Const.ContentsPATH).child(String.valueOf(mQuestion.getGenre())).child(mQuestion.getQuestionUid()).child(Const.AnswersPATH);
+        mAnswerRef.addChildEventListener(mEventListener);
+
+        //佐野が追加した
+        mSwitch = (Switch)findViewById(R.id.switch1);
+
+        //Switch保持のために追加
+        mPreference = PreferenceManager.getDefaultSharedPreferences(this);
+        //Switch保持のために追加
+
+        mSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                mSwitch = (Switch)findViewById(R.id.switch1);
+
+                if(mSwitch.isChecked()) {
+                    Log.v("スイッチ", "ON");
+                    //Switch保持のために追加
+                    SharedPreferences.Editor editor = mPreference.edit();
+                    editor.putBoolean("スイッチ",true);
+                    editor.commit();
+                    //Switch保持のために追加
+                    Toast.makeText(QuestionDetailActivity.this, "お気に入りに追加しました。", Toast.LENGTH_SHORT).show();
+                } else {
+                    Log.v("スイッチ", "OFF");
+                    //Switch保持のために追加
+                    SharedPreferences.Editor editor = mPreference.edit();
+                    editor.putBoolean("スイッチ",false);
+                    editor.commit();
+                    //Switch保持のために追加
+                    Toast.makeText(QuestionDetailActivity.this, "お気に入りから削除しました。", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        //佐野が追加した
+
     }
 }
